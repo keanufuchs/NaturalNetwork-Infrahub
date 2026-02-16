12f7adfb9eca86848ce07ee6266c157e825c2fb1
keanufuchs <kn.fch@gmail.com>
2026-02-16 19:30:45 +0100

    Update GraphQL query for DcimSwitch to accept a name parameter and modify switch hostname transformation logic

--- transforms/switch_hostname.py
+++ transforms/switch_hostname.py
@@ -5,8 +5,7 @@ class SwitchHostnameTransform(InfrahubTransform):
     query = "dcim_switch_names"
 
     async def transform(self, data):
-        switch = self.nodes[0]
-        switch_name = switch.name.value
-        return (
-            f"hostname {switch_name}",
-        )
+        nodes = self.nodes
+        for node in nodes:
+            print(node.name.value)
+        # return nodes
